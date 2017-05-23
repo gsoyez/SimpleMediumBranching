097c4eecffaa8b0cfefb0a74277f85d487e4e88a
Linnea Grans Samuelsson <linnea_g_s@hotmail.com>
2017-05-23 15:46:02 +0200

    Added first files

---
+++ independent-version/Linnea/event.cc
@@ -0,0 +1,180 @@
+#include "event.hh"
+#include <gsl/gsl_math.h>
+#include <iostream>
+#include <gsl/gsl_integration.h>
+
+using std::cout;
+using std::endl;
+
+///Initializing Particle with values that will signify that
+///this has no physical meaning. E.g. first particle
+///will not have a parent and will get parent = -1.
+Particle::Particle(int parent,double startTime,double x,double endTime,
+        int child1, int child2){
+    setParent(parent);
+    setStartTime(startTime);
+    setX(x);
+    setEndTime(endTime);
+    setChild1(child1);
+    setChild2(child2);
+}
+//############################################
+///Defining member functions for the class Branching
+
+
+/// default ctor
+Branching::Branching(int init_seed): _t(-1), _z(-1){
+    _r = gsl_rng_alloc (gsl_rng_default);
+    setSeed(init_seed);
+}
+
+/// default dtor
+Branching::~Branching(){
+    gsl_rng_free(_r);
+}
+
+///Using simplified kernel g >= real kernel f in the veto algorithm
+/// g = 1/(z(1-z))^(3/2)
+/// f = (1-z(1-z))^(5/2)/(z(1-z))^(3/2)
+void Branching::generateBranching(double x, double cutoff){
+    double z=1,R=1,fgratio=0,t=0;
+    cutoff=cutoff/x;
+    ///Calculating the integral from cutoff to 1-cutoff
+    double alpha = (4-8*cutoff)/sqrt(cutoff*(1-cutoff));
+
+    while (fgratio<R){
+        ///Generate t according to g
+        t = t - log(gsl_rng_uniform(_r))/(alpha/(2*sqrt(x)));
+        ///Generate z according to g. x-dependence of alpha cancels
+        double u = gsl_rng_uniform(_r);
+        double v = (4*cutoff-2)/sqrt(cutoff*(1-cutoff))+u*alpha*0.5;
+        ///z distribution is symmetric around 0.5. Generate below.
+        z = 0.5 + v/(2*sqrt(v*v+16));
+        ///Decide if rejecting or not
+        fgratio = pow((1-z*(1-z)),2.5); ///Rejection factor f/g
+        R = gsl_rng_uniform(_r);
+    }
+    _t=t;
+    _z=z;
+}
+
+
+void Branching::setSeed(int new_seed){
+	_seed=new_seed;
+	gsl_rng_set (_r, _seed);
+}
+//##############################################
+///Defining member functions for the class Event
+
+Event::Event(){
+    setCutoff(0.01); ///<default cutoff, gives _alpha around 40
+}
+
+void Event::setCutoff(double cutoff){
+    _cutoff = cutoff;
+}
+
+void Event::generateEvent(double time){
+    ///-1 is the default parent, 0 is starting time, x is energy fraction
+    ///TODO make a default parent and use, to not rely on using -1
+    _particles.clear();
+	Particle firstParticle(-1,0);
+    firstParticle.setX(1);
+    _particles.push_back(firstParticle);
+    _endTime=time;
+	_branch();
+}
+
+unsigned int Event::getN() const{
+	return (_particles.size()+1)/2;///<Number of final particles
+}
+
+const std::vector<Particle>& Event::getParticles() const{
+	return _particles;
+}
+
+///Branches the last particle in _particles
+void Event::_branch(){
+    unsigned int parent=_particles.size()-1;
+    double x=_particles[parent].x();///<Relies on pushing back child before branching
+    if(x<2*_cutoff){
+        return;///<We have reached the bottom of the recursion => we have one particle.
+    }
+  	///splittingTime, randomly generated
+  	_v.generateBranching(x, _cutoff);
+  	double splittingTime = _v.t();
+  	double z = _v.z();
+  	double timeLeft=_endTime - _particles[parent].startTime();
+    if(timeLeft < splittingTime){
+        return;///<We have reached the bottom of the recursion => we have one particle.
+    }
+    ///No if triggered =>we have time enough for another split and x is large enough
+    double currentTime=_particles[parent].startTime() + splittingTime;
+    _particles[parent].setEndTime(currentTime);
+    ///To add to the vector storing the event
+    Particle child1(parent,currentTime,z*x), child2(parent,currentTime,(1-z)*x);
+    ///Branching into two
+    ///First child:
+    _particles.push_back(child1);
+    _particles[parent].setChild1(_particles.size()-1);
+    _branch();
+    ///Second child:
+    _particles.push_back(child2);
+    _particles[parent].setChild2(_particles.size()-1);
+    _branch();
+}
+
+
+
+
+//##############################################
+///Defining member functions for the class GenerateInMedium
+
+GenerateInMedium::GenerateInMedium(){
+}
+
+void GenerateInMedium::generateEvent(double time,double cutoff){
+    ///-1 is the default parent, 0 is starting time, x is energy fraction
+    ///TODO make a default parent and use, to not rely on using -1
+    _event.particles.clear();
+	Particle firstParticle(-1,0);
+    firstParticle.setX(1);
+    _event.particles.push_back(firstParticle);
+    _endTime=time;
+    _cutoff=cutoff;
+	_branch();
+
+	///Event.set(stuff such as particles)
+}
+
+
+///Branches the last particle in _particles
+void GenerateInMedium::_branch(){
+    unsigned int parent=_event.particles.size()-1;
+    double x=_event.particles[parent].x();///<Relies on pushing back child before branching
+    if(x<2*_cutoff){
+        return;///<We have reached the bottom of the recursion => we have one particle.
+    }
+  	///splittingTime, randomly generated
+  	_v.generateBranching(x, _cutoff);
+  	double splittingTime = _v.t();
+  	double z = _v.z();
+  	double timeLeft=_endTime - _event.particles[parent].startTime();
+    if(timeLeft < splittingTime){
+        return;///<We have reached the bottom of the recursion => we have one particle.
+    }
+    ///No if triggered =>we have time enough for another split and x is large enough
+    double currentTime=_event.particles[parent].startTime() + splittingTime;
+    _event.particles[parent].setEndTime(currentTime);
+    ///To add to the vector storing the event
+    Particle child1(parent,currentTime,z*x), child2(parent,currentTime,(1-z)*x);
+    ///Branching into two
+    ///First child:
+    _event.particles.push_back(child1);
+    _event.particles[parent].setChild1(_event.particles.size()-1);
+    _branch();
+    ///Second child:
+    _event.particles.push_back(child2);
+    _event.particles[parent].setChild2(_event.particles.size()-1);
+    _branch();
+}
---
+++ independent-version/Linnea/event.hh
@@ -0,0 +1,117 @@
+#ifndef EVENT_HH_INCLUDED
+#define EVENT_HH_INCLUDED
+
+#include <vector>
+#include "gsl/gsl_rng.h"
+
+class Particle{
+public:
+    Particle(int parent=-1,double startTime=-1,double x=-1,double endTime=-1,
+        int child1=-1, int child2=-1);
+    ///Set and get parent particle
+    void setParent(int value) { _parent=value;}
+    int parent() const { return _parent;}
+    ///Set and get time when particle is created through branching
+    void setStartTime(double value) { _startTime=value;}
+    double startTime() const { return _startTime;}
+    ///Set and get x(energy fraction compared to leading particle)
+    void setX(double value) { _x=value;}
+    double x() const { return _x;}
+    ///Set and get time when particle branches
+    void setEndTime(double value) { _endTime=value;}
+    double endTime() const { return _endTime;}
+    ///Set and get the child particles the particle branches into
+    void setChild1(double value) { _child1=value;}
+    void setChild2(double value) { _child2=value;}
+    int child1() const { return _child1;}
+    int child2() const { return _child2;}
+
+private:
+    ///All these are initialized to -1
+    int _parent; ///< Not unsigned, want -1 to be an option for the first one
+    double _startTime;
+    double _x; ///<Fraction of energy compared to leading particle
+    double _endTime;
+    int _child1;
+    int _child2;
+};
+
+
+/// \class Branching
+/// Generate random branching times and z values
+/// The class uses GSL for random numbers.
+class Branching{
+public:
+  /// default ctor
+  Branching(int init_seed=1);
+  /// default dtor
+  ~Branching();
+  void generateBranching(double x, double cutoff);
+  double t() const {return _t;}
+  double z() const {return _z;}
+  /// get the random seed
+  int seed() const { return _seed;}
+  /// set the random seed
+  void setSeed(int new_seed);
+
+private:
+  gsl_rng *_r; ///< random number generator (GSL)
+  int _seed;   ///< random seed
+  double _t;
+  double _z;
+};
+
+
+/// \class Event
+///Generate an event that has branched for a
+///given time, which is given as input
+class Event{
+public:
+    Event();
+    void setCutoff(double cutoff);
+    double getCutoff() const {return _cutoff;}
+    void generateEvent(double time);///nope
+	unsigned int getN() const;
+	const std::vector<Particle>& getParticles() const;
+	///need setParticles
+	void setSeed(int newSeed){_v.setSeed(newSeed);}///nope
+    int getSeed(){return _v.seed();}///nope
+    std::vector<Particle> particles;///<public for now, consider options
+    const std::vector<Particle>& testParticles() const {return particles;}///temporary test
+private:
+    std::vector<Particle> _particles;
+    void _branch();///nope
+    Branching _v;///nope
+    double _cutoff;
+    double _endTime;
+};
+
+
+
+///\class GenerateInMedium
+///Generate an event. Returns a member of the class Event.
+class GenerateInMedium{
+public:
+    GenerateInMedium();
+    ///Return the generated event:
+    void generateEvent(double time, double cutoff);
+    const Event event() const {return _event;}
+
+	void setSeed(int newSeed){_v.setSeed(newSeed);}
+    int getSeed(){return _v.seed();}
+    Event _event; ///should not be public but want to check things
+private:
+
+    void _branch();
+    Branching _v;
+    double _cutoff;
+    double _endTime;
+};
+
+
+
+
+
+
+
+#endif // EVENT_HH_INCLUDED
