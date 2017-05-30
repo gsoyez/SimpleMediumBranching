a8be387e662577630697729d8a6b2d56cf9c3f6c
Linnea Grans Samuelsson <linnea_g_s@hotmail.com>
2017-05-30 11:00:22 +0200

    Added example code for checking veto algorithm results

--- common-version/main.cc
+++ common-version/main.cc
@@ -2,6 +2,7 @@
 #include "event.hh"
 #include "generator.hh"
 #include <iomanip>
+#include <gsl/gsl_math.h>
 using namespace std;
 
 
@@ -36,6 +37,58 @@ int main(){
   for (unsigned int i = 0; i < final_x.size(); i++ ) {
     cout << final_x[i] <<endl;
   }
+
+
+
+
+
+
+
+  ///Checking bin close to 0.5 and first bin in time
+  cout << "Checking fluctuations, takes less than a minute at current number of iterations" <<endl;
+  Branching v;
+  double cutoff = 1e-10;
+  double alpha = (4-8*cutoff)/sqrt(cutoff*(1-cutoff));///<integral of simplified kernel
+  unsigned int iter1=1e4;///usually 1e3 is good
+  unsigned int iter2=1e4;///<adjust to follow behaviour
+
+  double totalArea=399993.0/2;
+  double expectedZ=0.0389833*iter2/totalArea;
+  double expectedT=0.000199977*iter2;
+  double deviationZ=0;
+  double deviationT=0;
+
+  double binZ,binT;
+  for (unsigned int i=1; i<iter1;i++){
+    binZ=0;
+    binT=0;
+      for (unsigned int j=1; j<iter2;j++){
+        v.generateBranching(1,cutoff);
+        double z=v.z();
+        double t=v.t();
+          if (z>0.49&&z<0.5){
+            binZ+=1;
+          }
+          if (t<10*cutoff){
+            binT+=1;
+          }
+      }
+      deviationZ += (binZ-expectedZ)*(binZ-expectedZ);
+      deviationT += (binT-expectedT)*(binT-expectedT);
+
+  }
+
+  cout << setprecision(4) <<"deviationZ : " << sqrt(deviationZ/iter1) << " root n " << sqrt(expectedZ) << endl;
+  cout << setprecision(4) <<"deviationT : " << sqrt(deviationT/iter1) << " root n " << sqrt(expectedT) << endl;
+
+
+
+
+
+
+
+
+
 
   return 0;
 }
